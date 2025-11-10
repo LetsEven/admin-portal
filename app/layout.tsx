b6353c4ed5d501f7913802622e53dd4ebf3ed636
Ezequiel Lino <elino@xquisito.ai>
2025-11-10 17:25:38 -0600

    redirect en layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider 
-      signUpFallbackRedirectUrl="/"
-      signInFallbackRedirectUrl="/"
+    <ClerkProvider
+      signUpForceRedirectUrl="/"
+      signInForceRedirectUrl="/"
     >
       <RestaurantProvider>
         <html lang="es">
